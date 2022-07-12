8490f17bcecc4f3f8d9f58e4a8c9bca7fe9a3149
AlphaCyber-X <105292203+AlphaCyber-X@users.noreply.github.com>
2022-07-12 18:47:49 +0530

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -1,11 +1,11 @@
 function successfullMessage(msg) {
-    return "✅ *AQUA*:  ```" + msg + "```"
+    return "✅ *Devil*:  ```" + msg + "```"
 }
 function errorMessage(msg) {
-    return "🛑 *AQUA*:  ```" + msg + "```"
+    return "🛑 *Devil*:  ```" + msg + "```"
 }
 function infoMessage(msg) {
-    return "⏺️ *AQUA*:  ```" + msg + "```"
+    return "⏺️ *Devil*:  ```" + msg + "```"
 }
 
 
